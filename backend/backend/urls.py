bcf85d3bbc63e1f3b9d6111652d9bcf232561638
kunalarya873 <kunalarya873@gmail.com>
2024-09-07 16:36:43 +0530

    adde some more functionality

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from stocks.views import StockViewSet
+from stocks.views import *
 
 router = DefaultRouter()
 router.register(r'stocks', StockViewSet)
@@ -9,4 +9,5 @@ router.register(r'stocks', StockViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('', home, name="home")
 ]
